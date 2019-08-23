7a6e41dda130c4bdd07aa8665ff6299576b68fcb
Joseph Haddon <jdhaddon@gmail.com>
2019-08-23 18:01:26 -0400

    tweak

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -151,7 +151,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = zooAnimals.map((zooAnimal, index, zooAnimals) => {
+const lowCaseAnimalNames = zooAnimals.map((zooAnimal) => {
   return zooAnimal.animal_name.toLowerCase();
 });
 
